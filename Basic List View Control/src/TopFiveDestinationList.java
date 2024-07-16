fbc99028db5ec48912c4ebb4e17de957f23ad14f
Faizah <168539677+functionfaiecho@users.noreply.github.com>
2024-07-17 00:50:20 +1200

    Complete

--- Basic List View Control/src/TopFiveDestinationList.java
+++ Basic List View Control/src/TopFiveDestinationList.java
@@ -5,53 +5,55 @@ import java.net.URI;
 import javax.swing.*;
 import javax.swing.border.*;
 
+// Entry point for the TopFiveDestinationList application
 public class TopFiveDestinationList {
     public static void main(String[] args) {
+        // Use SwingUtilities to ensure the UI updates happen on the Event Dispatch Thread
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                // Create and display the main application frame
                 TopDestinationListFrame topDestinationListFrame = new TopDestinationListFrame();
                 topDestinationListFrame.setTitle("Top 5 Destination List");
-                topDestinationListFrame.getContentPane().setBackground(Color.decode("#FAE3C6"));
+                topDestinationListFrame.getContentPane().setBackground(Color.decode("#FAE3C6")); // Set background color
                 topDestinationListFrame.setVisible(true);
             }
         });
     }
 }
 
-@SuppressWarnings("serial")
+// JFrame subclass to represent the main window of the application
 class TopDestinationListFrame extends JFrame {
     private DefaultListModel<TextAndIcon> listModel;
 
     public TopDestinationListFrame() {
         super("Top Five Destination List");
-
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setSize(900, 750);
+        setSize(900, 750); // Set the initial size of the frame
 
         listModel = new DefaultListModel<>();
         
-        //Label with my name on it.
-        
+        // Label with my name.
         JLabel nameLabel = new JLabel("Created by Faizah");
         nameLabel.setFont(new Font("Arial", Font.PLAIN, 14));
         nameLabel.setHorizontalAlignment(SwingConstants.CENTER);
         nameLabel.setBorder(new EmptyBorder(10, 0, 10, 0));
-        
-        // Clickable links to "the most popular travel package". 
 
+        // Add destination items to the list
         addDestinationNameAndPicture("1. Auckland, New Zealand", "The multi-cultural and vibrant financial capital of New Zealand.", new ImageIcon(getClass().getResource("/resources/Auckland.jpg")), "https://www.newzealand.com/nz/auckland/");
         addDestinationNameAndPicture("2. Singapore", "Lovingly referred to as the 'Little Red Dot' and home to the world's best airport.", new ImageIcon(getClass().getResource("/resources/Singapore.jpg")), "https://www.visitsingapore.com/en/");
         addDestinationNameAndPicture("3. Dubai, United Arab Emirates", "This urban jungle is the place to be for all your shopping needs and memories to last a lifetime.", new ImageIcon(getClass().getResource("/resources/Dubai.jpg")), "https://www.italia.it/en/lazio/rome");
         addDestinationNameAndPicture("4. Vienna, Austria", "The hills are alive, with the sound of music.", new ImageIcon(getClass().getResource("/resources/Vienna.jpg")), "https://www.austria.info/en/where-to-go/cities/vienna");
 
         JList<TextAndIcon> list = new JList<>(listModel);
-        // Background colour has changed.
-        list.setBackground(Color.decode("#FAE3C6"));
+        list.setBackground(Color.decode("#FAE3C6")); // Set list background color
         JScrollPane scrollPane = new JScrollPane(list);
-        scrollPane.getViewport().setBackground(Color.decode("#A9e4ef"));
+        scrollPane.getViewport().setBackground(Color.decode("#A9e4ef")); // Set scroll pane background color
 
-        TextAndIconListCellRenderer renderer = new TextAndIconListCellRenderer(10, 10, 10, 10); // Add padding here
+        // Set custom renderer for list items with padding
+        TextAndIconListCellRenderer renderer = new TextAndIconListCellRenderer(10, 10, 10, 10);
         list.setCellRenderer(renderer);
+        
+        // Add mouse listener for click events on list items
         list.addMouseListener(new MouseAdapter() {
             @Override
             public void mouseClicked(MouseEvent e) {
@@ -60,6 +62,7 @@ class TopDestinationListFrame extends JFrame {
                 TextAndIcon item = (TextAndIcon) list.getModel().getElementAt(index);
                 if (Desktop.isDesktopSupported()) {
                     try {
+                        // Open the URL in the default browser
                         Desktop.getDesktop().browse(new URI(item.getUrl()));
                     } catch (Exception ex) {
                         ex.printStackTrace();
@@ -68,17 +71,20 @@ class TopDestinationListFrame extends JFrame {
             }
         });
 
+        // Set layout and add components to the frame
         getContentPane().setLayout(new BorderLayout());
         getContentPane().add(nameLabel, BorderLayout.NORTH);
         getContentPane().add(scrollPane, BorderLayout.CENTER);
     }
 
+    // Helper method to add destinations to the list model
     private void addDestinationNameAndPicture(String name, String description, Icon icon, String url) {
         TextAndIcon tai = new TextAndIcon(name, description, icon, url);
         listModel.addElement(tai);
     }
 }
 
+// Class to encapsulate destination data
 class TextAndIcon {
     private String name;
     private String description;
@@ -109,6 +115,7 @@ class TextAndIcon {
     }
 }
 
+// Custom renderer for list items to display text and icon
 class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer<TextAndIcon> {
     private static final Border NO_FOCUS_BORDER = new EmptyBorder(1, 1, 1, 1);
     private Border insideBorder;
@@ -129,9 +136,11 @@ class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer<Tex
     @Override
     public Component getListCellRendererComponent(JList<? extends TextAndIcon> list, TextAndIcon value,
                                                   int index, boolean isSelected, boolean hasFocus) {
+        // Set the text with HTML to include the description and link
         setText("<html><a href='" + value.getUrl() + "'>" + value.getName() + "</a><br/><span style='font-weight: normal;'>" + value.getDescription() + "</span></html>");
         setIcon(value.getIcon());
 
+        // Change background and foreground colors based on selection state
         if (isSelected) {
             setBackground(list.getSelectionBackground());
             setForeground(list.getSelectionForeground());
@@ -140,6 +149,7 @@ class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer<Tex
             setForeground(list.getForeground());
         }
 
+        // Set border based on focus state
         Border outsideBorder = hasFocus ? UIManager.getBorder("List.focusCellHighlightBorder") : NO_FOCUS_BORDER;
         setBorder(BorderFactory.createCompoundBorder(outsideBorder, insideBorder));
 
@@ -150,6 +160,7 @@ class TextAndIconListCellRenderer extends JLabel implements ListCellRenderer<Tex
         return this;
     }
 
+    // Override methods to improve performance
     @Override
     public void validate() {}
     @Override
