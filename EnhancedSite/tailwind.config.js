fcc9bb43f5f6315633ab130f96b5841f73235a9c
Faizah <168539677+functionfaiecho@users.noreply.github.com>
2024-10-07 20:44:23 +1300

    Colours update

--- EnhancedSite/tailwind.config.js
+++ EnhancedSite/tailwind.config.js
@@ -1,9 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'], // Add paths to your components
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: '#FEF9EF',
+        linkedText: '#FE6D73',
+        text: '#114b5f',
+      },
+    },
   },
   plugins: [],
 }
-
