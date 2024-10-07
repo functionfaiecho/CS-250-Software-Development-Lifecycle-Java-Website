4452690344c00b0ab4ddd17c54a602af0d0ecd37
Faizah <168539677+functionfaiecho@users.noreply.github.com>
2024-10-07 20:44:44 +1300

    Introducing header

--- EnhancedSite/enhancedjava/app/(components)/Header.tsx
+++ EnhancedSite/enhancedjava/app/(components)/Header.tsx
@@ -2,8 +2,8 @@
 
 const Header: React.FC = () => {
   return (
-    <div>
-      
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100px' }}>
+      <h1 style={{ fontSize: '3rem', fontWeight: 'bold' }}>Top Five Countries List</h1>
     </div>
   );
 };
