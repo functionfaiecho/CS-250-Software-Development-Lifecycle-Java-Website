ebb56d278dc0150906e25dab81a34effdbb6dc14
Faizah <168539677+functionfaiecho@users.noreply.github.com>
2024-10-08 08:57:58 +1300

    Fix margin error

--- EnhancedSite/enhancedjava/app/(components)/Header.tsx
+++ EnhancedSite/enhancedjava/app/(components)/Header.tsx
@@ -3,7 +3,7 @@
 const Header: React.FC = () => {
   return (
     <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '150px' }}>
-      <h1 style={{ fontSize: '3rem', fontWeight: 'bold', marginBottom: '5px' }}>Top Five Countries List</h1>
+      <h1 style={{ fontSize: '3rem', fontWeight: 'bold', marginBottom: '2px' }}>Top Five Countries List</h1>
       <h2 style={{ fontSize: '1.5rem', fontStyle: 'italic', marginTop: '0' }}>(in CS 250 anyway.)</h2>
     </div>
   );
